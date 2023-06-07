766d8a7bf8c292000b3bc27793322c0bc99f21b5
Maszyna1337 <luna.sielawa@gmail.com>
2023-06-07 18:27:58 +0200

    1

--- kolokwium/poprawakol.py
+++ kolokwium/poprawakol.py
@@ -25,12 +25,12 @@ import seaborn as sns
 # x = np.arange(-3, 5, 0.5)
 # y = pow(x, 2) + (np.sin(pow(x,2))/ x)
 #
-# plt.plot(x, y, label='f(x)', linestyle="-", color="red")
+# plt.plot(x, y, "r^", label="f(x)")
 #
 # plt.xlabel('x')
 # plt.ylabel('Wyniki Funkcji')
 #
-# plt.axis([-3, 5, 0.5, 1])
+# plt.axis([-3, 5, -1, 25])
 #
 # plt.legend()
 #
@@ -38,35 +38,35 @@ import seaborn as sns
 # plt.savefig("Dominika_Sielawa_zad2.png")
 # plt.show()
 # Zadanie3
+# #
+# df = pd.read_csv('handel.csv', header=0, sep=',', decimal='.')
+# wartosc = df.groupby('Wyszczegolnienie')['Wartosc'].sum()
 #
-# sns.set()
+# plt.pie(wartosc.index, wartosc.values)
+#
+# for i, value in enumerate(wartosc.values):
+#     plt.text(i, value, str(value), ha='center', va='bottom')
+#
+# plt.title('Wartosc wyszczegolnionych')
+# plt.xlabel('Wyszczegolnione')
+# plt.ylabel('Wartosc')
+# plt.legend()
+# # plt.savefig("Dominika_Sielawa_zad3.png")
+# # plt.show()
+#
+# # Zadanie4
 #
 # df = pd.read_csv('handel.csv', header=0, sep=',', decimal='.')
+# wartosc = df.groupby('Wyszczegolnienie')['Wartosc'].sum()
+# ax = plt.subplots()
+# plt.pie(wartosc.index)
 #
-# wartosc_stacji = df.groupby('Rok')['Wartosc']
-#
-# plt.bar(wartosc_stacji.rok, wartosci_stacji.wartosc)
-#
-# for i, value in enumerate(wartosc_stacji.values):
+# for i, value in enumerate(wartosc.values):
 #     plt.text(i, value, str(value), ha='center', va='bottom')
-# plt.xlabel('Wartosc stacji pawliw na przeciagu kilku lat')
-# plt.title("Wykres slupkowy z Handel.csv")
-# plt.savefig("Dominika_Sielawa_zad3.png")
+#
+# plt.title('Wartosc wyszczegolnionych')
+# plt.xlabel('Wyszczegolnione')
+# plt.ylabel('Wartosc')
+# plt.legend()
+# plt.savefig("Dominika_Sielawa_zad4.png")
 # plt.show()
-
-# Zadanie4
-
-df = pd.read_csv('handel.csv', header=0, sep=',', decimal='.')
-wartosc = df.groupby('Wyszczegolnienie')['Wartosc'].sum()
-
-plt.pie(wartosc.index, wartosc.values)
-
-for i, value in enumerate(wartosc.values):
-    plt.text(i, value, str(value), ha='center', va='bottom')
-
-plt.title('Wartosc wyszczegolnionych')
-plt.xlabel('Wyszczegolnione')
-plt.ylabel('Wartosc')
-plt.legend()
-plt.savefig("Dominika_Sielawa_zad4.png")
-plt.show()
